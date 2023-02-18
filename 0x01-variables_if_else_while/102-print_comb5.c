7dd0daede4530f96a6fea0fc93fc283eca247381
Kopano <--replace-all>
2023-02-17 16:31:21 -0800

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			for (num3 = num1; num3 <= 9; num3++)
+			{
+				for (num4 = num2 + 1; num4 <= 9; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ')
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
