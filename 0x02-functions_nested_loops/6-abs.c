7aea6536d4b42a76e0fef32fb75c5341810cabf4
Kopano <--replace-all>
2023-02-21 14:07:20 -0800

    absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - computes the absolute value of the interger
+ *
+ *  @n: the interger to compute the absolute value of
+ *
+ *  Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
