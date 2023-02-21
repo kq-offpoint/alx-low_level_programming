3e0dd75098a09177cca05c1458440ecc365cb0ab
Kopano <--replace-all>
2023-02-21 15:30:51 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= 9; J++)
+		{
+			result - i * j;
+			if (result <= 9)
+			{
+				_puchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
