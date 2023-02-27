999c710fcd7f2874ea1616a24319f76d6e095901
Kopano <--replace-all>
2023-02-27 13:58:21 -0800

    .

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * puts2 - prints every other character of a string
- * @str: the string to print
+ * puts2 - prints characters in a string
+ * @str: string
+ * Return: nothing
  */
+
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[1] != '\0'; i += 2)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
+		i += 2;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
