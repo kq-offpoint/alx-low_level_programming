a7a88f8f7bbf92e00fb5fbb0582633e2eaae6aa1
Kopano <--replace-all>
2023-03-02 14:36:29 -0800

    Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Encode a string to 1337.
+ * @str: the string to be encoded.
+ *
+ * Return: a pointer to the encoded string.
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[i])
+	{
+		for (i = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] || str[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+		i++;
+	}
+
+	return (str);
+}
