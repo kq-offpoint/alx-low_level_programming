f29f9d2f998f68c15014679ea893b2481544ab76
Kopano <--replace-all>
2023-03-02 13:49:37 -0800

    always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letter of
+ * a string to uppercase
+ * @str: string to be changed.
+ *
+ * Return: a pointer to the changed string.
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
