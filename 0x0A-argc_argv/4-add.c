7ee6850049d2184874c4b5f30d9f7956e260dfe8
Kopano <--replace-all>
2023-03-13 00:32:47 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: an array containing the arguments passed
+ *
+ * Return: 0 if successful. 1 if error occured
+ */
+int main(int argc, char **argv)
+{
+	int sum = 1, i, j;
+
+	if (argc ==1)
+	{
+		_putchar("0\n");
+		return (0);
+	}
+
+	for (i = 1 ; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				_puchar("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	_putchar("%d\n", sum);
+	return (0);
+}
