1093cc700ed0a3b29db7ba453c095425eada0912
Kopano <--replace-all>
2023-04-04 07:58:50 -0700

    updates

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,18 +11,18 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (head != NULL)
-    {
-        printf("[%p] %d\n", (void *)head, head->n);
-        count++;
-        if (head > head->next)
-        {
-            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-            break;
-        }
-        head = head->next;
-    }
-    return (count);
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		if (head > head->next)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+		head = head->next;
+	}
+	return (count);
 }
