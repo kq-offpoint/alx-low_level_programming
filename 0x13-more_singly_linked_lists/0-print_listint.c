d9c0c5b0d2193480480f7b2990ce657de486bd8d
Kopano <--replace-all>
2023-04-04 06:32:51 -0700

    0-Print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: pointer to the head of the list
- * Return: the number of nodes
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-    return (count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
